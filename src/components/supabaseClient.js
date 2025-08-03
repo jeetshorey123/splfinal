88d24fb94d32a162311eee837f94305278c90ceb
Jeet Shorey <132572002+jeetshorey123@users.noreply.github.com>
2025-08-03 13:50:41 +0530

    change2

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-id.supabase.co';
-const supabaseKey = 'your-anon-or-service-role-key'; // Use env variables in production
+const supabaseUrl = 'https://wnwabwghxbjtmdbujxjz.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indud2Fid2doeGJqdG1kYnVqeGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NzcwNzQsImV4cCI6MjA2OTQ1MzA3NH0.jf6ADsIRXJYuw_8KLfyHgoj8vRDjfndlpVH68VPFF-c'; // Use env variables in production
 export const supabase = createClient(supabaseUrl, supabaseKey);
